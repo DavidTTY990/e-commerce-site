c8525f10cc570991be97ac123bb8b3862b854840
David Tsai <>
2023-04-28 18:41:37 +0800

    [Edit] 新增全域數字千分號

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 import loadingEffect from 'vue-loading-overlay';
 import 'vue-loading-overlay/dist/css/index.css';
+import { currency } from './methods/filters';
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
+app.config.globalProperties.$filters = {
+  currency,
+};
 app.use(VueAxios, axios);
 app.use(router);
 app.component('loadingEffect', loadingEffect); // 全域註冊該套件，不同於 app.use 為安裝套件
